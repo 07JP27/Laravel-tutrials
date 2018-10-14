af449cced9698baaa8acaef73e59dae464a7736b
07JP27 <jynpei.t@gmail.com>
2018-10-14 14:28:34 +0900

    3-1P66

--- resources/views/hello/index.php
+++ resources/views/hello/index.php
@@ -9,6 +9,6 @@ h1{font-size:120pt; text-align:right; color:#fafafa; margin:-50px 0px -120px 0px
 <body>
 <h1>Index</h1>
 <p><?php echo $msg; ?></p>
-<p><?php echo date("Y年n月j日"); ?></p>
+<p>ID=<?php echo $id; ?></p>
 </body>
 </html>
