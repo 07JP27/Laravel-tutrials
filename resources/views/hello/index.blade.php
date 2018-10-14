7f3572b1899d04868458492b3769255ed2bf1fce
07JP27 <jynpei.t@gmail.com>
2018-10-14 15:46:32 +0900

    3-4P91

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,25 +1,17 @@
-<html>
-<head>
-<title>Hello/Index</title>
-<style>
-body{font-size:16pt; color:#999;}
-h1{font-size:120pt; text-align:right; color:#fafafa; margin:-50px 0px -120px 0px;}
-</style>
-</head>
-<body>
-<h1>Blade/Index</h1>
-<p>whileの例</p>
-<ol>
-@php
-$counter = 0;
-@endphp
+@extends('layouts.helloapp')
 
-@while($counter < count($data))
-<li>{{$data[$counter]}}</li>
-@php
-$counter++;
-@endphp
-@endwhile
-</ol>
-</body>
-</html>
+@section('title','Index')
+
+@section('menubar')
+ @parent
+ インデックスページ
+@endsection
+
+@section('content')
+ <p>ここが本文のコンテンツです。</p>
+ <p>必要なだけ記述できます。</p>
+@endsection
+
+@section('footer')
+copyright 2017 hogehoge.
+@endsection
