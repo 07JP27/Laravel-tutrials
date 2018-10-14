808e92147aa96ad4e1f4665724f5854b4bd2c17a
07JP27 <jynpei.t@gmail.com>
2018-10-14 16:28:05 +0900

    3-5P105

---
+++ app/Http/Composer/HelloComposer.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Http\Composer;
+
+use Illuminate\View\View;
+
+class HelloComposer
+{
+    public function compose(View $view)
+    {
+        $view->with('view_message', 'this view is"' . $view->getName() . '"!!');
+    }
+}
