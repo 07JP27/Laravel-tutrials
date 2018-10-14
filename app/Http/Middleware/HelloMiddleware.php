1e82f390f16b5f9b76690f3893e0056ce680a136
07JP27 <jynpei.t@gmail.com>
2018-10-14 18:32:42 +0900

    4-1P117

--- app/Http/Middleware/HelloMiddleware.php
+++ app/Http/Middleware/HelloMiddleware.php
@@ -15,12 +15,13 @@ class HelloMiddleware
      */
     public function handle($request, Closure $next)
     {
-        $data =[
-            ['name'=>'山田たろう','mail'=>'taro@yamada'],
-            ['name'=>'田中はなこ','mail'=>'hanako@tanaka'],
-            ['name'=>'鈴木さちこ','mail'=>'sachiko@suzuki']
-        ];
-        $request->merge(['data'=>$data]);
-        return $next($request);
+        $response = $next($request);
+        $content = $response->content();
+        $pattern = '/<middleware>(.*)<\/middleware>/i';
+        $replace = '<a href="http://$1">$1</a>';
+        $content = preg_replace($pattern, $replace, $content);
+
+        $response->setContent($content);
+        return $response;
     }
 }
