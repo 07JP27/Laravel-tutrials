705f87779bbf70eccbc3e5aef7c16819a9276297
07JP27 <jynpei.t@gmail.com>
2018-10-16 22:24:17 +0900

    6-2P245

--- app/Person.php
+++ app/Person.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use App\Scopes\ScopePerson;
 
 class Person extends Model
 {
@@ -11,9 +12,7 @@ class Person extends Model
     {
         parent::boot();
 
-        static::addGlobalScope('age', function(Builder $builder){
-            $builder->where('age', '>', 20);
-        });
+        static::addGlobalScope(new ScopePerson);
     }
 
     public function getData()
